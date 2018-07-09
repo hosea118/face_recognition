44ad263e826d19797673baae082834e4d65af656
haoqing <qhao@aibee.com>
2018-07-09 14:59:53 +0800

    use model=cnn to find faces

--- photo_assistant/recognize_faces_in_pictures.py
+++ photo_assistant/recognize_faces_in_pictures.py
@@ -11,11 +11,13 @@ def face_recognition_in_pic(file_name):
 
     unknown_image = face_recognition.load_image_file(file_name)
     try:
-        unknown_face_encoding = face_recognition.face_encodings(unknown_image)
-        if len(unknown_face_encoding) > 1:
+        face_bounding_boxes = face_recognition.face_locations(unknown_image, number_of_times_to_upsample=0, model='cnn')
+        if len(face_bounding_boxes) > 1:
             return 'more_faces'
+        elif len(face_bounding_boxes) == 0:
+            return 'no_face'
         else:
-            unknown_face_encoding = unknown_face_encoding[0]
+            unknown_face_encoding = face_recognition.face_encodings(unknown_image, known_face_locations=face_bounding_boxes)[0]
     except IndexError:
         logger.error('no face find in the image')
         return 'no_face'
