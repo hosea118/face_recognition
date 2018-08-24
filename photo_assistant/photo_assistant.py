3c16e017b6445a2aa70d4d4f3e9a396b3db6c712
haoqing <qhao@aibee.com>
2018-08-24 18:44:48 +0800

    classify video files

--- photo_assistant/photo_assistant.py
+++ photo_assistant/photo_assistant.py
@@ -11,6 +11,7 @@ classify_photo_by_date = 0
 
 support_photo = ['jpg', 'jpeg', 'JPG', 'png', 'PNG']
 ignore_path = ['.git', 'category_result', 'log']
+video_suffix = ['MOV', 'mp4', 'MP4', 'mov', 'HEIC']
 global logger
 
 def copy_file(dst, src):
@@ -59,11 +60,29 @@ if __name__ == '__main__':
             dirs[:] = [d for d in dirs if d not in ignore_path]
             for file in files:
                 #如果文件是图片则处理
+                file_path = os.path.join(fpath, file)
                 if file.split('.')[-1] in support_photo:
-                    file_path = os.path.join(fpath, file)
+                    #file_path = os.path.join(fpath, file)
                     if classify_photo_by_date:
                         classify_by_date(out_dir, file_path)
                     else:
                         classify_by_face(out_dir, file_path)
+                elif file.split('.')[-1] in video_suffix:
+                    video_out_dir = out_dir + '/video_files'
+                    if not os.path.exists(video_out_dir):
+                        os.mkdir(video_out_dir)
+                    dst = video_out_dir + '/' + file
+                    print('dst is ' + dst)
+                    shutil.copyfile(file_path, dst)
+                else:
+                    unknown_out_dir = out_dir + '/unknown_files'
+                    if not os.path.exists(unknown_out_dir):
+                        os.mkdir(unknown_out_dir)
+                    dst = unknown_out_dir + '/' + file
+                    print('dst is ' + dst)
+                    if os.path.isfile(file):
+                        shutil.copyfile(file_path, dst)
+                    elif os.path.isdir(file):
+                        shutil.copytree(file_path, dst)
 
 
